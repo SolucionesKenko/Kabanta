4a21c59d6aac21869444aa2857267d78cbe923d7
Mac Air <mac@Miguels-MacBook-Air-2.local>
2023-05-15 14:04:18 -0600

    Agregar funciones ECG simulados y  Generados

--- Proy_graph/data_test.py
+++ Proy_graph/data_test.py
@@ -2,8 +2,18 @@ import sys
 import numpy as np
 import os
 import wfdb
-record = wfdb.rdrecord('/Users/mac/Documents/AlanBecarioSW/PruebasQt/Proy_graph/Signals/00001_lr',channels=[0,1,2],sampfrom=0)
+import scipy.signal
+import neurokit2 as nk
+record = wfdb.rdrecord('/Users/mac/Documents/AlanBecarioSW/PruebasQt/Kabanta/Proy_graph/Signals/00001_lr',channels=[0,1,2],sampfrom=0)
+ecg12 = nk.ecg_simulate(duration=10, method="multileads")
+#ecg12 = list(ecg12)
+
 signal = record.p_signal
 ecg_signal = signal.tolist()
 print(ecg_signal[3])
+#Cambios de Codigo Main, Separacion de StyleSheet, y agregado de Generacion de senales con neurokit
+
+
+
+
 
