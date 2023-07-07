7e4fc35499b401a75bd8b99675c5cc7ba453c90f
Snaider Bautista <>
2023-07-07 14:55:10 -0600

    spo

--- Proy_graph/main.py
+++ Proy_graph/main.py
@@ -8,6 +8,7 @@ import sys
 import numpy as np
 from data_test import ecg_signal, obtainSignals
 from window import Ui_window
+from collections import deque
 
 # Manejo de puertos
 import serial 
@@ -17,6 +18,7 @@ from ESPmsg import ParserState, WorkerThread, State
 from serialCoder import SerialCoder
 
 from enum import Enum, auto 
+import spo
 # Manejo de arreglos en la senal 
 # todo, cambiar el manejo de datos con collections deque
 
@@ -55,6 +57,7 @@ class MainWindow(QtWidgets.QWidget):
         self.mi_diccionario = {HEART_RATE:0,TEMPERATURE:0,SPO:0,SYSPRESSURE:0,DIAPRESSURE:0,FR:0, CO:0}
         self.generateSig = 0
         self.ecg12 = 0
+        self.i = 0
         
         self.parserState = ParserState.Type
         self.crc = 0
@@ -71,6 +74,7 @@ class MainWindow(QtWidgets.QWidget):
         self.rate = 0
         self.channelMask = 0
         self.messageInBytes = []
+        self.spo = spo.SPO()
         #self.timer=qtc.QTimer()
 
         # Connections
@@ -93,6 +97,7 @@ class MainWindow(QtWidgets.QWidget):
         self.custom_crc_table = {}
         self.poly = 0x04C11DB7
         self.generate_crc32_table(self.poly)
+        self.r = deque()
 
         #Button Control
         self.Ui_window = Ui_window()
@@ -130,6 +135,7 @@ class MainWindow(QtWidgets.QWidget):
         self.initSignalGrahps()
         #Actualizacion de grafica
         self.adder = 0
+        self.i_rsp = 0
         self.timer = QtCore.QTimer()
         # User code starts Here 
 
@@ -148,6 +154,8 @@ class MainWindow(QtWidgets.QWidget):
         self.channel2 = [-2 for i in self.x]
         self.channel3 = [-4 for i in self.x]
         #todo pen =
+        self.data_line_ppg = self.ui.plt.plot()
+        self.data_line_rsp = self.ui.plt.plot()
         self.data_line_channel1 = self.ui.plt.plot(self.x,self.channel1)
         self.data_line_channel2 = self.ui.plt.plot(self.x,self.channel2)
         self.data_line_channel3 = self.ui.plt.plot(self.x,self.channel3)
@@ -157,8 +165,15 @@ class MainWindow(QtWidgets.QWidget):
         # todo cambiar el manejo de datos con collections deque
         if(self.adder >= len(self.ecg12['I'])-1):
             self.adder = 0
+        if(self.i_rsp >= 9999):
+            self.i_rsp = 0
+        while(self.i > 499):
+            self.i = self.i - 1
+            self.r.popleft()
+
         self.adder = self.adder + 1
-        
+        self.i_rsp = self.i_rsp + 1
+        self.i = self.i + 1
         self.x = self.x[1:]  # Remove the first y element.
         self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
 
@@ -166,14 +181,22 @@ class MainWindow(QtWidgets.QWidget):
         self.channel1.append(self.ecg12['I'][self.adder])   # Add a new random value.
 
         self.channel2 = self.channel2[1:]                   # Remove the first
-        self.channel2.append(self.ecg12['II'][self.adder]-2)# Add a new random value.
+        self.channel2.append(self.ecg12['II'][self.adder]+4)# Add a new random value.
 
         self.channel3 = self.channel3[1:] 
-        self.channel3.append(self.ecg12["III"][self.adder]-4)  # Add a new random value.
+        self.channel3.append(self.ecg12["III"][self.adder]+8)  # Add a new random value.
 
-        self.data_line_channel1.setData(self.x, self.channel1)  # Update the data.
+        self.r.append((self.rsp[self.i_rsp])+12)
+        print(self.r)
+
+        self.spo.update_plot()
+
+        self.data_line_rsp.setData(self.x, self.r)
+        self.data_line_ppg.setData(self.x, list(self.spo.dataIR)[1:])
+        self.data_line_channel1.setData(self.x, self.channel1)
         self.data_line_channel2.setData(self.x, self.channel2)
         self.data_line_channel3.setData(self.x, self.channel3)
+        
     def displayHello(self):
         print("hello")
 
@@ -221,6 +244,8 @@ class MainWindow(QtWidgets.QWidget):
             self.setDefaultValues()
             self.generateSig = obtainSignals()
             self.ecg12 = self.generateSig.generateSignals(self.mi_diccionario[HEART_RATE])
+            self.rsp = list(self.generateSig.generate_rsp())
+            print(self.rsp)
 
         if(self.signalState != SignalState.Playing):
             self.ui.heartRateValue_Label.setText(str(self.mi_diccionario[HEART_RATE]))
