f7e07b1f8ef23ed3226f60eb3dda0abfe2b9b888
Alan Arredondo <alanalexander1112@gmail.com>
2023-11-02 17:41:34 -0600

    Actualización de la clase de Gpios

--- Proy_graph/gpios.py
+++ Proy_graph/gpios.py
@@ -27,43 +27,61 @@ class LEDstate(IntEnum):
     ON = 1
     OFF = 0
 
-boardPinout = [3,5,7,11,13,15]
-BMCPinout = [2,3,4,17,17,22]
-
-def init_Gpios():
-    ### specify Raspberry Pi Pin numbering
-    GPIO.setmode(GPIO.BOARD)            # BOARD numbering system
-    #GPIO.setmode(GPIO.BCM)             # BCM numbering system
-    pinoutMode = GPIO.getmode()
-    ### Setup up GPIO pin
-    if pinoutMode == PinoutMode.BOARD:
-        pinout = boardPinout
-    elif pinoutMode == PinoutMode.BMC:
-        pinout = BMCPinout
-    else:
-        print("Error setting Raspberry GPIO mode")
+class GPIOS():
+    def __init__(self):
+        self.boardPinout = [3,5,7,11,13,15]
+        self.BMCPinout = [2,3,4,17,17,22]
+        ### specify Raspberry Pi Pin numbering
+        GPIO.setmode(GPIO.BOARD)            # BOARD numbering system
+        #GPIO.setmode(GPIO.BCM)             # BCM numbering system
     
-    # Configure GPIO pins
-    # To configure a channel as an input output:
-    ## https://sourceforge.net/p/raspberry-gpio-python/wiki/Inputs/
-    GPIO.setup(pinout[Gpios.LED], GPIO.OUT)        # input
-    GPIO.setup(pinout[Gpios.SHOCK1:], GPIO.IN)       # output
+    def __del__(self):
+        self.clearGPIOS()
 
-    # Default LED State
-    GPIO.output(pinout[Gpios.LED], LEDstate.OFF)
+    def clearGPIOS():
+        #### To clean up at the end of your script:
+        # Only clean up GPIO channels that your script has used. Note that GPIO.cleanup() 
+        # also clears the pin numbering system in use.
+        GPIO.cleanup()
 
-def clearGPIOS():
-    #### To clean up at the end of your script:
-    # Only clean up GPIO channels that your script has used. Note that GPIO.cleanup() 
-    # also clears the pin numbering system in use.
-    GPIO.cleanup()
-    
-def detectPinoutMode():    # detecting numbering system
-    pinoutMode = GPIO.getmode()
-    print("BOARD numbering system = 10")
-    print("BMC numbering system = 11")
-    print("The GPIO Pin mode is " + str(pinoutMode))
-    print("To see details of the pinout use the command <pinout> in the terminar")
+    def init_Gpios(self):
+        self.pinoutMode = GPIO.getmode()
+        ### Setup up GPIO pin
+        if self.pinoutMode == PinoutMode.BOARD:
+            self.pinout = self.boardPinout
+        elif self.pinoutMode == PinoutMode.BMC:
+            self.pinout = self.BMCPinout
+        else:
+            print("Error setting Raspberry GPIO mode")
+        
+        # Configure GPIO pins
+        # To configure a channel as an input output:
+        ## https://sourceforge.net/p/raspberry-gpio-python/wiki/Inputs/
+        GPIO.setup(self.pinout[Gpios.LED], GPIO.OUT)        # input
+        GPIO.setup(self.pinout[Gpios.SHOCK1:], GPIO.IN)       # output
+
+        # Default LED State
+        GPIO.output(self.pinout[Gpios.LED], LEDstate.OFF)
+        self.setEventDetects()
+
+    def setEventDetects(self):
+        GPIO.add_event_detect(self.pinout[Gpios.SHOCK1:],GPIO.RISING,bouncetime=200)
+
+        GPIO.add_event_callback(self.pinout[Gpios.SHOCK1], self.onShockButttonPressed())
+        # GPIO.add_event_callback(self.pinout[Gpios.SHOCK2], self.onShockButttonPressed())
+        # GPIO.add_event_callback(self.pinout[Gpios.CHARGE], self.onChargeButtonPressed())
+        # GPIO.add_event_callback(self.pinout[Gpios.UPENERGY], self.onUpEnergyButtonPressed())
+       # GPIO.add_event_callback(self.pinout[Gpios.DOWNENERGY], self.onDownEnergyButtonPressed())
+
+
+        
+    def detectPinoutMode(self):    # detecting numbering system
+        self.pinoutMode = GPIO.getmode()
+        print("BOARD numbering system = 10")
+        print("BMC numbering system = 11")
+        print("The GPIO Pin mode is " + str(self.pinoutMode))
+        print("To see details of the pinout use the command <pinout> in the terminar")
+
 
 
 # to see more interacción with the gpios you can see the input documentation:
